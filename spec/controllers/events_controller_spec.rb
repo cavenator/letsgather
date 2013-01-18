e4e17e0e6cc29fe3e357810d79c750f7c7696411
cavenator <cavenator@gmail.com>
2013-01-17 22:49:52 -0800

    Finally figured out how to do some rspec controller tests with devise

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -1,10 +1,31 @@
 require 'spec_helper'
+require "devise/test_helpers"
 
 describe EventsController do
 	include Devise::TestHelpers
 
+	def login
+		@request.env["devise.mapping"] = Devise.mappings[:user]
+		@user = FactoryGirl.create(:user,:email=>"testperson@gmail.com")
+		sign_in @user
+	end
 	context "list of events index page" do
-		it "should be able to see list of created events"
+
+		it "should be able to see list of created events" do
+			login
+			get 'index'
+		end
+
+		it "should be able to create new event" do
+			login
+			event = mock_model(Event)
+			Event.stub(:new).and_return(event)
+			get 'new'
+		end
+
+		after(:all) do
+			User.destroy_all
+		end
 	end
 
 end
