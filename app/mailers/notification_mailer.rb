3a183783630b1b0831b8f595fe484d9e8f381f03
cavenator <cavenator@gmail.com>
2013-09-17 17:47:36 -0700

    Added cohosts to notifications

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,19 +4,22 @@ class NotificationMailer < ActionMailer::Base
 	def notify_host_of_attending_guest(attendee, event)
 		@attendee = attendee
 		@event = event
-		mail(to: event.user.email, subject: "Invitation accepted for event #{event.name}")
+		hosts_email = event.get_hosts.map{|host| host.email}
+		mail(to: hosts_email, subject: "Invitation accepted for event #{event.name}")
 	end
 
 	def notify_host_of_declining_guest(attendee, event)
 		@attendee = attendee
 		@event = event
-		mail(to: event.user.email, subject: "Invitation declined for event #{event.name}")
+		hosts_email = event.get_hosts.map{|host| host.email}
+		mail(to: hosts_email, subject: "Invitation declined for event #{event.name}")
 	end
 
 	def notify_host_of_all_guest_changes(attendee, differences, event)
 		@attendee = attendee
 		@differences_hash = differences
 		@event = event
-		mail(to: event.user.email, subject: "Response changed: #{event.name}")
+		hosts_email = event.get_hosts.map{|host| host.email}
+		mail(to: hosts_email, subject: "Response changed: #{event.name}")
 	end
 end
