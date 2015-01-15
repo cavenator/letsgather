f80633192e7996f09835eb9ffe31ce12502466da
AJ Cave <acave@cj.com>
2015-01-15 12:58:55 -0800

    Updated database.yml for postgres-docker connection and updated script to remove 'rake db:create' command

--- runDocker.sh
+++ runDocker.sh
@@ -1,7 +1,6 @@
-echo "creating db"
-rake db:create
-echo "building db"
+echo "if you haven't done so already, create the database for this env"
+echo "migrating db"
 rake db:migrate
 echo "starting server"
 foreman start
-echo "ruby server running"
+echo "foreman running"
